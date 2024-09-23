cf5926c3c8165778d47b0aaf22be1e9b7d5a8209
JMProf <132485731+JMProf@users.noreply.github.com>
2024-09-23 19:15:06 +0200

    Create ObtenerNumerosPares.java

---
+++ RepasoInicial/ObtenerNumerosPares.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class NumerosPares{
+
+    // Atributos
+    private int min;
+    private int max;
+
+    // Constructor
+    public NumerosPares(int min, int max){
+        this.min = min;
+        this.max = max;
+    }
+
+    // Recorre los números entre min y max buscando los pares
+    public void obtenerNumerosPares(){
+        System.out.println("Los números pares entre " + min + " y " + max + " son:");
+        for(int i=min; i<=max; i++){
+            if(i%2 == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
+public class ObtenerNumerosPares {
+    public static void main(String[] args) {
+        int min, max;
+
+        // Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Introducir números
+        System.out.println("Introduce el número min:");
+        min = sc.nextInt();
+        System.out.println("Introduce el número max:");
+        max = sc.nextInt();
+
+        // Crear objeto de la clase pedida y lanzar el método para obtener los pares
+        NumerosPares np = new NumerosPares(min, max);
+        np.obtenerNumerosPares();
+    }
+}
