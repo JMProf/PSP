34a22f2b04be38acb2d3d138072cf997019ec065
JMProf <132485731+JMProf@users.noreply.github.com>
2024-10-15 17:08:11 +0200

    Update UsaHilo2.java

--- UD2. Programación multihilo/UsaHilo2.java
+++ UD2. Programación multihilo/UsaHilo2.java
@@ -1,19 +1,19 @@
 class HiloSimple2 implements Runnable{
-	@Override
-	public void run(){
-	  	for(int i=0; i<10;i++){
-	    		System.out.println("Dentro del hilo");
-		}
-	}
+    @Override
+    public void run(){
+        for(int i=0; i<10;i++){
+	    System.out.println("Dentro del hilo");
+        }
+    }
 }
 
 public class UsaHilo2{
-	public static void main(String[] args){
-	  	HiloSimple2 hs = new HiloSimple2();
-	  	Thread t = new Thread(hs);
-	  	t.start();
-	  	for(int i=0;i<10;i++){
-	      		System.out.println("Fuera del hilo");
-		}
-	}
+    public static void main(String[] args){
+        HiloSimple2 hs = new HiloSimple2();
+        Thread t = new Thread(hs);
+        t.start();
+        for(int i=0;i<10;i++){
+	    System.out.println("Fuera del hilo");
+        }
+    }
 }
